a6c5d7b2daac52d4f201151e0ea89244ee1ab402
Mama GDG <mamsogdg@gmail.com>
2024-04-18 15:53:21 +0000

    first commit

--- index.js
+++ index.js
@@ -1,133 +1,80 @@
-const display1El = document.querySelector(".display-1");
-const display2El = document.querySelector(".display-2");
-const tempResultEl = document.querySelector(".temp-result");
-const numbersEl = document.querySelectorAll(".number");
-const operationEl = document.querySelectorAll(".operation");
-const equalEl = document.querySelector(".equal");
-const clearAllEl = document.querySelector(".all-clear");
-const clearLastEl = document.querySelector(".last-entity-clear");
-let dis1Num = "";
-let dis2Num = "";
-let result = null;
-let lastOperation = "";
-let haveDot = false;
-
-numbersEl.forEach((number) => {
-  number.addEventListener("click", (e) => {
-    if (e.target.innerText === "." && !haveDot) {
-      haveDot = true;
-    } else if (e.target.innerText === "." && haveDot) {
-      return;
-    }
-    dis2Num += e.target.innerText;
-    display2El.innerText = dis2Num;
-    // console.log();
+$(function() {
+  let headerElem = $('header');
+  let logo = $('#logo');
+  let navMenu = $('#nav-menu');
+  let navToggle = $('#nav-toggle');
+  $('#properties-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<a href="#" class="slick-arrow slick-prev">previous</a>',
+    nextArrow: '<a href="#" class="slick-arrow slick-next">next</a>',
+    responsive: [{
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 530,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      }
+    ]
   });
-});
-
-operationEl.forEach((operation) => {
-  operation.addEventListener("click", (e) => {
-    if (!dis2Num) return;
-    haveDot = false;
-    const operationName = e.target.innerText;
-    if (dis1Num && dis2Num && lastOperation) {
-      mathOperation();
+  $('#testimonials-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<a href="#" class="slick-arrow slick-prev"><</a>',
+    nextArrow: '<a href="#" class="slick-arrow slick-next">></a>'
+  });
+  navToggle.on('click', () => {
+    navMenu.css('right', '0');
+  });
+  $('#close-flyout').on('click', () => {
+    navMenu.css('right', '-100%');
+  });
+  $(document).on('click', (e) => {
+    let target = $(e.target);
+    if (!(target.closest('#nav-toggle').length > 0 || target.closest('#nav-menu').length > 0)) {
+      navMenu.css('right', '-100%');
+    }
+  });
+  $(document).scroll(() => {
+    let scrollTop = $(document).scrollTop();
+    if (scrollTop > 0) {
+      navMenu.addClass('is-sticky');
+      logo.css('color', '#000');
+      headerElem.css('background', '#fff');
+      navToggle.css('border-color', '#000');
+      navToggle.find('.strip').css('background-color', '#000');
     } else {
-      result = parseFloat(dis2Num);
+      navMenu.removeClass('is-sticky');
+      logo.css('color', '#fff');
+      headerElem.css('background', 'transparent');
+      navToggle.css('bordre-color', '#fff');
+      navToggle.find('.strip').css('background-color', '#fff');
     }
-    clearVar(operationName);
-    lastOperation = operationName;
-    console.log(result);
+    headerElem.css(scrollTop >= 200 ? {
+      'padding': '0.5rem',
+      'box-shadow': '0 -4px 10px 1px #999'
+    } : {
+      'padding': '1rem 0',
+      'box-shadow': 'none'
+    });
   });
+  $(document).trigger('scroll');
 });
-function clearVar(name = "") {
-  dis1Num += dis2Num + " " + name + " ";
-  display1El.innerText = dis1Num;
-  display2El.innerText = "";
-  dis2Num = "";
-  tempResultEl.innerText = result;
-}
-
-function mathOperation() {
-  if (lastOperation === "x") {
-    result = parseFloat(result) * parseFloat(dis2Num);
-  } else if (lastOperation === "+") {
-    result = parseFloat(result) + parseFloat(dis2Num);
-  } else if (lastOperation === "-") {
-    result = parseFloat(result) - parseFloat(dis2Num);
-  } else if (lastOperation === "/") {
-    result = parseFloat(result) / parseFloat(dis2Num);
-  } else if (lastOperation === "%") {
-    result = parseFloat(result) % parseFloat(dis2Num);
-  }
-}
-// operation();
-
-equalEl.addEventListener("click", () => {
-  if (!dis2Num || !dis1Num) return;
-  haveDot = false;
-  mathOperation();
-  clearVar();
-  display2El.innerText = result;
-  tempResultEl.innerText = "";
-  dis2Num = result;
-  dis1Num = "";
-});
-
-clearAllEl.addEventListener("click", () => {
-  dis1Num = "";
-  dis2Num = "";
-  display1El.innerText = "";
-  display2El.innerText = "";
-  result = "";
-  tempResultEl.innerText = "";
-});
-
-clearLastEl.addEventListener("click", () => {
-  display2El.innerText = "";
-  dis2Num = "";
-});
-
-window.addEventListener("keydown", (e) => {
-  if (
-    e.key === "0" ||
-    e.key === "1" ||
-    e.key === "2" ||
-    e.key === "3" ||
-    e.key === "4" ||
-    e.key === "5" ||
-    e.key === "6" ||
-    e.key === "7" ||
-    e.key === "8" ||
-    e.key === "9" ||
-    e.key === "."
-  ) {
-    clickButtonEl(e.key);
-    // console.log(e.key)
-  } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "%") {
-    clickOperation(e.key);
-  } else if (e.key === "*") {
-    clickOperation("x");
-    // console.log(e.key)
-  } else if (e.key == "Enter" || e.key === "=") {
-    clickEqual();
-  }
-  // console.log(e.key)
-});
-function clickButtonEl(key) {
-  numbersEl.forEach((button) => {
-    if (button.innerText === key) {
-      button.click();
-    }
-  });
-}
-function clickOperation(key) {
-  operationEl.forEach((operation) => {
-    if (operation.innerText === key) {
-      operation.click();
-    }
-  });
-}
-function clickEqual() {
-  equalEl.click();
-}
